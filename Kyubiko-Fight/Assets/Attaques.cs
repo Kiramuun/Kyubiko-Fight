6aa1c86ea23653e9c5c254da159640e7d83339b2
KokoChii <145577589+KokoChii@users.noreply.github.com>
2023-11-16 14:37:15 +0100

    premire scene
    scene de test

---
+++ Kyubiko-Fight/Assets/Attaques.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attaque : MonoBehaviour
+{
+    public Animator _animatorRef;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !_animatorRef.GetBool("attackSplash"))
+        {
+            _animatorRef.SetBool("attackSplash", true);
+        }
+
+        if (Input.GetButtonDown("Fire2") && !_animatorRef.GetBool("attackFireball"))
+        {
+            _animatorRef.SetBool("attackFireball", true);
+        }
+
+        if (Input.GetButtonDown("Fire3") && !_animatorRef.GetBool("Ultimate"))
+        {
+            _animatorRef.SetBool("Ultimate", true);
+        }
+    }
+}
